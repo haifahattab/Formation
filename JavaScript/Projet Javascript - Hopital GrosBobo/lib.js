93fe1422bf5b563dc4a26070bf8bd511e6cfa71a
haifahattab <hattab_haifa@yahoo.com>
2020-08-23 18:53:44 +0200

    Declencher un évenement avec Jquery

--- JavaScript/Projet Javascript - Hopital GrosBobo/lib.js
+++ JavaScript/Projet Javascript - Hopital GrosBobo/lib.js
@@ -1,13 +1,22 @@
-//Ajouter un élement p qui contient les définitions de chaque composant du corp.
+//Afficher le paragraphe à partir son id et faire appel à la fonction deleteALLP.
 function showContent(pNumber) {
     deleteAllP(pNumber);
     var p = document.getElementById('text' + pNumber)
     p.style.display = "block"
 }
-
+//Cacher le paragraphe à partir son id.
 function deleteAllP(pNumber) {
     var hideP = document.getElementsByTagName("p");
     for (var i = 0; i < hideP.length; i++) {
         hideP.item(i).style.display = "none";
     }
 }
+//Evenement sur le bouton avec Jquery.
+$('button').on({
+    click: function() {
+        $(this).css('backgroundColor', 'red');
+    },
+    mouseleave: function() {
+        $(this).css('backgroundColor', 'rgba(151, 148, 148, 0.8)');
+    }
+});

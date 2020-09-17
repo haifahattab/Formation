b897a72d2f140c63c8910bafa4db02b6a198a7ef
haifahattab <hattab_haifa@yahoo.com>
2020-09-17 14:29:03 +0200

    Niveau 2 PHP

--- PHP/Niveau 2/PHP-Exo5-Conjugaison/conjugaison.php
+++ PHP/Niveau 2/PHP-Exo5-Conjugaison/conjugaison.php
@@ -6,20 +6,31 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-        function conjuguer($str){
-            $str= "";
-            $n = strlen($str);
-            for($i=$n-1; $i<= $n-2; $i--){
-                $test = $str[i];
-                echo $test;
-            }
-            //La chaîne passée en paramètre ne doit pas dépasser 15 caractères ni contenir d'espaces.
-            // Vérifiez que la chaîne passée en paramètre se termine bien par "er".
-            if(strlen($str <= 15) && $str != " ") && $test = "er"){
-                str_replace($test);
-            }
+<?php 
+function  conjuguer($verbe) {
+    //La chaîne passée en paramètre ne doit pas dépasser 15 caractères ni contenir d'espaces.
+    // Vérifiez que la chaîne passée en paramètre se termine bien par "er".
+     if(preg_match('/^[^ ]{0,14}er$/', $verbe)) {
+        $newVerb = preg_replace('/er$/', '', $verbe);
+        echo '<p>Je '.$newVerb.'e</p>';
+        echo '<p>Tu '.$newVerb.'es</p>';
+        echo '<p>Il '.$newVerb.'e</p>';
+        if(preg_match('/g$/', $newVerb)) {
+            echo '<p>Nous '.$newVerb.'eons</p>';
+        } else {
+            echo '<p>Nous '.$newVerb.'ons</p>';
         }
-    ?>
+        echo '<p>Vous '.$newVerb.'ez</p>';
+        echo '<p>Ils '.$newVerb.'ent</p>';
+    } else {
+        echo '<p>Le verbe doit être un verbe du premier groupe de moins de 15 lettres et ne doit pas contenir d\'espace.</p>';
+    }
+};
+echo '<div align="center">';
+conjuguer("sortir");
+conjuguer("programmer");
+conjuguer("manger");
+echo '</div>';
+?>
 </body>
 </html>

c7e00ed74e3db5f23561235a4bf697a1ec96bb22
haifahattab <hattab_haifa@yahoo.com>
2020-10-27 15:44:06 +0100

    exo3

--- MySQL/Niveau_2/Exercice 3 - Vraie page login/login.php
+++ MySQL/Niveau_2/Exercice 3 - Vraie page login/login.php
@@ -1,3 +1,68 @@
+<?php        
+    if (isset($_POST['submit']))
+        {
+            $login = $_POST['login'];
+            $password = $_POST['password'];
+            if (!empty($login) && !empty($password))
+                {
+                    $bdd = new PDO('mysql:host=localhost;dbname=niv2-bdd;charset=utf8', 'root', '');
+                    $req = $bdd->prepare('SELECT * FROM connexions WHERE login = ? AND password = ?');
+                    $req-> execute(array(
+                        $login,
+                        $password));
+                    $resultat = $req->fetch(PDO::FETCH_ASSOC);
+                    session_start();
+                    if ($resultat || password_verify($password, $resultat['password']))
+                    {
+                        $_SESSION["login"] = $login;
+                        header("location: home.php");
+                    }
+                    else
+                    {
+                      
+                        if(!isset($_SESSION['nombre']))
+                            {
+                            // Initialisation de la variable
+                            $_SESSION['nombre'] = 1;
+                            // Blocage pendant 15 min
+                            $_SESSION['timestamp_limite'] = time() + 60*15;
+                            }
+                        if($_SESSION['nombre'] <= 5)
+                        {
+                            echo '<script type="text/javascript">';
+                            echo 'alert("Cet utilisateur existe déja dans la base de données! ")';
+                            echo  '</script>';                            
+                            $_SESSION['nombre']++;
+                        }
+                        // Si on a dépassé les 5 tentatives
+                        else
+                        {
+                            // Si le cookie marqueur n'existe pas on le crée                                     
+                            if(!isset($_COOKIE['marqueur']))
+                                    {
+                                    $timestamp_marque = time() + 60; // On le marque pendant une minute
+                                                $cookie_vie = time() + 606024; // Durée de vie de 24 heures pour le décalage horaire
+                                    setcookie("marqueur", $timestamp_marque, $cookie_vie);
+                                    }
+
+                            // on quitte le script
+                            exit();
+                        }
+                                    
+                    }
+         
+                }
+            else
+                {
+                    echo '<script type="text/javascript">';
+                    echo 'alert ("Renseignez votre email et votre mot de passe")';
+                    echo '</script>';
+                }
+            
+        }
+
+        ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,31 +74,18 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+       form{
+            margin-left: 40%;
+            margin-top: 10%;
+        }
+    </style>
     
 </head>
 <body>
-    <?php
-    if(isset($_POST['submit'])){
-        $login = $_POST['login'];
-        $password = $_POST['password'];
-
-        $base = new PDO('mysql:host=localhost;dname=niv2_bdd;charset=utf8','root','');
-        $sql = $base->prepare("SELECT login, password FROM connexions WHERE login = $login");
-        $sql->execute();
-        $data = $sql->fetch();
-        if(!$data){
-            echo "Vous n'étes pas inscris";
-        }else if{
-            $sql->bindParam(':password', $password);
-            $sql->execute();
-            $data = $sql->fetch();
-
-        }
-    }
-    ?>
-
+   
+    <form method="post" action="login.php">
     <h3>Inscription</h3>
-    <form method="post" action="#">
         <div class="form-group">
             <label for="ilogin">Login :</label>
             <input type="email" name="login" id="ilogin" placeholder="melanie@yahoo.com">
@@ -42,8 +94,8 @@
             <label for="ipassword">Mot de passe :</label>
             <input type="text" name="password" id="ipassword">
         </div>
-        <button name="submit">Se connecter</button>
-        <button name="inscrire">S'inscrire</button>
+        <button name="submit">Valider</button>
+        
     </form>
     
 </body>
